1a34053a19884460a4bcc8502988b0f3462ad88f
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-03-27 15:10:50 +0900

    feat : 레이아웃, 플렉스박스

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    // RN에서는 h2나 div 요소를 못 쓴다. -> 왜? DOM 요소를 가지고 있지 않아서..
+    // 또한 CSS에서 border라는 키가 존재하지 않음
+    return (
+        <View style={styles.appContainer}>
+            <View>
+                <TextInput placeholder="이름을 입력해주세요" />
+                <Button title="저장" />
+            </View>
+            <View>
+                <Text>목록</Text>
+                <Text>이름</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    appContainer: {
+        padding: 50
+    }
 });
