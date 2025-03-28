751f4b3d2f888f477843437009493d63ca5c8ccd
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-03-28 11:48:26 +0900

    feat: 목표 입력 기능 추가 및 스타일 개선

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
     // RN에서는 h2나 div 요소를 못 쓴다. -> 왜? DOM 요소를 가지고 있지 않아서..
     // 또한 CSS에서 border라는 키가 존재하지 않음
+
+    const [enteredGoalText, setEnteredGoalText] = useState("");
+
+    const goalInputHandler = (enteredText) => {
+        // 이벤트 처리하기
+        setEnteredGoalText(enteredText);
+    };
+
+    const addGoalHandler = () => {
+        console.log(enteredGoalText);
+    };
+
     return (
         <View style={styles.appContainer}>
-            <View>
-                <TextInput placeholder="이름을 입력해주세요" />
-                <Button title="저장" />
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="이름을 입력해주세요"
+                    onChangeText={goalInputHandler}
+                />
+                <Button title="저장" onPress={addGoalHandler} />
             </View>
-            <View>
+            <View style={styles.goalsContainer}>
                 <Text>목록</Text>
-                <Text>이름</Text>
             </View>
         </View>
     );
@@ -20,6 +36,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
     appContainer: {
-        padding: 50
+        flex: 1,
+        padding: 50,
+        paddingHorizontal: 16
+    },
+    inputContainer: {
+        flex: 1,
+        flexDirection: "row", // default는 column..? 일반 CSS하고는 다른듯
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: "#cccccc"
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: "#cccccc",
+        width: "70%",
+        marginRight: 8,
+        padding: 8
+    },
+    goalsContainer: {
+        flex: 5
     }
 });
